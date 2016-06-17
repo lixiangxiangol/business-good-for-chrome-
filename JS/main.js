5154693646008eeb89b98c83639f4a130ea94e29
lixiangxiangol <937487925@qq.com>
2016-06-17 17:59:21 +0800

    first

---
+++ JS/main.js
@@ -0,0 +1,20 @@
+function load() {
+    var box = document.getElementById("div1"), can = true;
+    box.innerHTML += box.innerHTML;
+    box.onmouseover = function () {
+        can = false
+    };
+    box.onmouseout = function () {
+        can = true
+    };
+    new function () {
+        var stop = box.scrollTop % 30 == 0 && !can;
+        if (!stop)box.scrollTop == parseInt(box.scrollHeight / 2) ? box.scrollTop = 0 : box.scrollTop++;
+        setTimeout(arguments.callee, box.scrollTop % 30 ? 10 : 1500);
+    }
+}
+
+$('#myTab a').click(function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+});
